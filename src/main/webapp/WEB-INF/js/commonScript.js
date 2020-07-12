9bb7817c26173ef44f519e0eb76ab22c9c85f194
rajanhhh <30908600+rajanhhh@users.noreply.github.com>
2020-07-12 20:58:39 +0530

    Delete commonScript.js

--- src/main/webapp/WEB-INF/js/commonScript.js
+++
@@ -1,7 +0,0 @@
-function logout(){
-	var form = document.createElement("form");
-	form.setAttribute('method',"post");
-	form.setAttribute('action',"/logout");
-	$("body").append(form); 
-	form.submit();
-}
