ac343ba6716e7839b19c64be3728aa6324fc861f
rajanhhh <rajeshkumarnayak1994@gmail.com>
2020-08-08 21:09:51 +0530

    checkin fresh code

--- src/main/java/com/form/JDBCHandler.java
+++ src/main/java/com/form/JDBCHandler.java
@@ -26,14 +26,14 @@ public class JDBCHandler {
 	
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/EMP";
-	//static final String DB_URL = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12348159";
+	//static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/EMP";
+	static final String DB_URL = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12348159";
 
 	//  Database credentials
-	static final String USER = "root";
-	static final String PASS = "rajan";
-	//static final String USER = "sql12348159";
-	//static final String PASS = "dEEwGHqICT";
+	//static final String USER = "root";
+	//static final String PASS = "rajan";
+	static final String USER = "sql12348159";
+	static final String PASS = "dEEwGHqICT";
 
 	public static void main(String[] args) {
 		Connection conn = null;
